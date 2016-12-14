6097ab4560ea9d040938981e847cc9e2a3c9d638
PakkuDon <pakku.pd@gmail.com>
2016-12-14 17:36:08 +1100

    Implement GET tags endpoint

--- routes/tags.js
+++ routes/tags.js
@@ -4,7 +4,17 @@ var db = require('../db');
 // GET /api/tags
 // Return filtered list of tags
 router.get('/', (req, res) => {
-  res.sendStatus(501);
+  db('tags').where('name', 'like', `%${req.query.tag || ''}%`)
+    .then(tags => {
+      res.json({
+        success: true,
+        tags: tags
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
