3ff45966414d1d13a6f689217779222c6677c27e
PakkuDon <pakku.pd@gmail.com>
2016-12-15 11:19:39 +1100

    Implement login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,35 @@
 var router = require('express').Router();
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 var db = require('../db');
 
 // POST /api/auth
 // Authenticate user
 router.post('/', (req, res) => {
-  res.sendStatus(501);
+  db('users').where({
+    email: req.body.email
+  })
+  .then(results => {
+    if (results.length === 0) {
+      res.sendStatus(404);
+    }
+    else {
+      var user = results[0];
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        var token = jwt.sign({ user_id: user.id }, req.app.get('secret'), {
+          expiresIn: '24h'
+        });
+
+        res.json({
+          success: true,
+          token: token
+        });
+      }
+      else {
+        res.sendStatus(401);
+      }
+    }
+  })
 });
 
 // DELETE /api/auth
