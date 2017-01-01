9ac2c0e45e768f273a62f0d64e87f14d02e5aa3c
PakkuDon <pakku.pd@gmail.com>
2017-01-02 01:52:00 +1100

    Change review routes to use Sequelize DB

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 var auth = require('../middleware/auth');
-var db = require('../knex_db');
+var { Review } = require('../models');
 
 // POST /api/reviews
 // Add review for recipe
@@ -11,31 +11,28 @@ router.post('/', auth.isAuthenticated, (req, res) => {
       message: 'Review data required'
     });
   }
-  var review = {
-    content: req.body.content,
-    score: parseInt(req.body.score),
-    date_posted: new Date(),
-    user_id: req.token.user_id,
-    recipe_id: req.body.recipe_id
-  }
 
   // Save review to database
-  db('reviews')
-    .insert(review)
-    .then(() => {
-      res.status(200).json({
-        success: true,
-        message: 'Review added successfully',
-        review: review
-      });
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({
-        success: false,
-        message: 'Internal server error'
-      });
+  Review.create({
+    content: req.body.content,
+    score: parseInt(req.body.score),
+    user_id: req.token.user_id,
+    recipe_id: req.body.recipe_id
+  })
+  .then(review => {
+    res.status(200).json({
+      success: true,
+      message: 'Review added successfully',
+      review
     });
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error'
+    });
+  });
 });
 
 module.exports = router;
