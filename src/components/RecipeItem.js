ab2797f633c05e996916d13886cad583be967790
PakkuDon <pakku.pd@gmail.com>
2017-01-13 17:30:25 +1100

    Change styling and layout for recipe items

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 
 export default (props) => (
   <div className='recipe-item card'>
+    <div className='recipe-image' style={{ backgroundImage: `url('${props.recipe.image_url}')` }}></div>
     <Link to={`/recipes/${props.recipe.id}`}>{props.recipe.name}</Link>
   </div>
 );
