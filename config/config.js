1b82d13e4153ea3df2a22978e83305affefc0424
PakkuDon <pakku.pd@gmail.com>
2017-01-17 15:40:19 +1100

    Add dialect to db production config

--- config/config.js
+++ config/config.js
@@ -9,6 +9,7 @@ module.exports = {
     "dialect": "postgresql"
   },
   "production": {
-    "url": process.env.DATABASE_URL
+    "url": process.env.DATABASE_URL,
+    "dialect": "postgresql"
   }
 }
